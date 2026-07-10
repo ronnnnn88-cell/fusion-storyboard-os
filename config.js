a5cd3bcde9db7b17681d4401a3feab5123e966b8
ronnnnn88-cell <ronnnnn88@gmail.com>
2026-07-11 03:03:04 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.FUSION_CONFIG = window.FUSION_CONFIG || {
-  GOOGLE_CLIENT_ID: "",
-  GOOGLE_API_KEY: "",
-  GOOGLE_APP_ID: ""
+  GOOGLE_CLIENT_ID: "550330232157-6ona74k56h20ivpju9lvvsc6jp6vhbik.apps.googleusercontent.com",
+  GOOGLE_API_KEY: "AIzaSyDssRudTxKayEaS_6oclaQeqWQr6WIK-W8",
+  GOOGLE_APP_ID: "550330232157"
 };
